2a728e0b9f9df0c6cc2c4e08c7b82922eeb5962d
vanka334 <isip_i.d.kuzin@mpt.ru>
2023-05-31 23:50:13 +0300

    do

---
+++ FuncLib(ser-deser)/SerDeser.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using Microsoft.Win32;
+using Newtonsoft.Json;
+using System.Security.Cryptography;
+
+namespace FuncLib_ser_deser_
+{
+    public class SerDeser
+    {
+        public static void  serialize<T>(T theme)
+        {
+
+            string desktop = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+            File.WriteAllText($"{desktop}\\theme.txt", theme.ToString());
+
+        }
+        public static T Deserialize<T>()
+        {
+            T theme;
+            string desktop = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+            if (File.Exists($"{desktop}\\theme.txt"))
+            {
+                string filik = File.ReadAllText($"{desktop}\\theme.txt");
+                theme = JsonConvert.DeserializeObject<T>(filik);
+                return theme;
+            }
+            else {
+              /*  File.Create($"{desktop}\\theme.txt");
+                
+                File.WriteAllText($"{desktop}\\theme.txt", "WPB");
+                string filik = File.ReadAllText($"{desktop}\\theme.txt");
+                theme = JsonConvert.DeserializeObject<T>(filik);*/
+              return default(T);
+            }
+        }
+            
+            
+        
+
+    }
+}
---
+++ styles2/MainWindow.xaml.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using FuncLib_ser_deser_;
+
+namespace styles2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+           
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            App.Theme = "BLM";
+
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            App.Theme = "WPB";
+
+        }
+    }
+    public partial class App : Application
+    {
+        private static string theme;
+        public static string Theme
+        {
+            get { return theme; }
+            set {
+                if (value != null)
+                {
+                    theme = value;
+                    var dict = new ResourceDictionary { Source = new Uri($"pack://application:,,,/WpfCustomControlLibrary1;component/Themes/{value}.xaml", UriKind.Absolute) };
+
+                    Current.Resources.MergedDictionaries.RemoveAt(0);
+                    Current.Resources.MergedDictionaries.Insert(0, dict);
+                    SerDeser.serialize(Theme);
+                }
+
+            }
+
+        }
+        public App() { 
+            InitializeComponent();
+            Theme = SerDeser.Deserialize<string>();
+        }
+
+    }
+}
